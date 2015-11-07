1d5b44b3e263f78b23aa91ab33aadb1d2f01c389
Clinton Wolfe <clinton@omniti.com>
2015-11-06 22:31:40 -0500

    Pin more things, and add test-kitchen - good build on El Capitan

--- omnibus-ansibledk/config/projects/ansibledk.rb
+++ omnibus-ansibledk/config/projects/ansibledk.rb
@@ -9,16 +9,28 @@ install_dir "#{default_root}/#{name}"
 build_version Omnibus::BuildVersion.semver
 build_iteration 1
 
-override :ruby, version: "2.1.7"
+# Version pinning.  Most of the software definitons have a
+# default_version of "master" (ie, floating on latest)
+override :ruby,           version: "2.1.7"
+override :bundler,        version: "1.10.6"
+override :rubygems,       version: "2.4.8"
+override :'test-kitchen', version: "v1.4.2"
 
 # Creates required build directories
 dependency "preparation"
 
 # ansibledk dependencies/components
+
+# Python land
 dependency "python"
 dependency "pip"
-dependency "ruby"
 dependency "ansible-dk"
+
+# Ruby land
+dependency "ruby"
+dependency "rubygems"
+dependency "bundler"
+dependency "test-kitchen"
 
 # Version manifest file
 dependency "version-manifest"
