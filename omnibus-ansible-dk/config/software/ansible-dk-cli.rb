b521e6691f0e46783a0d7bd1c35b7ae03abd4bf9
Clinton Wolfe <clinton@omniti.com>
2015-11-08 00:25:48 -0500

    Add click for future python-based ansible-dk CLI tool

--- omnibus-ansible-dk/config/software/ansible-dk-cli.rb
+++ omnibus-ansible-dk/config/software/ansible-dk-cli.rb
@@ -2,6 +2,9 @@ name "ansible-dk-cli"
 default_version "0.1.0" # This is just to silence a warning
 description "One day this might be a generic CLI tool; today it is a hack to make shell-init work"
 
+# This isn't used yet (as we're a shell script, derp) but will be soon!
+dependency 'click'
+
 build do
   copy Dir.pwd + "/../ansible-dk-cli/ansible-dk", "#{install_dir}/embedded/bin"
   copy Dir.pwd + "/../ansible-dk-cli/ansible-dk", "#{install_dir}/bin"  
