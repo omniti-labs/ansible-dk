0981e3480b7de2f0cc80863a3b060468feec08a9
Clinton Wolfe <clinton@omniti.com>
2015-11-11 12:31:16 -0500

    Hackish fix to a missing symlink in the serverspec repo

--- omnibus-ansible-dk/config/software/serverspec.rb
+++ omnibus-ansible-dk/config/software/serverspec.rb
@@ -21,6 +21,12 @@ build do
 
   bundle "install", env: env
 
+  # wercker.yml is a symlink into a submodule, used for testing.  Without the
+  # submodule, gem build breaks, because the link is broken.
+  # So, hackishly delete the link and touch an empty file.
+  delete "wercker.yml"
+  touch "wercker.yml"
+  
   gem "build serverspec.gemspec", env: env
   gem "install serverspec-*.gem" \
       " --no-ri --no-rdoc", env: env
