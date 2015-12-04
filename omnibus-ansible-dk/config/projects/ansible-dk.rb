7c6a5125b7ef7e37c18d2b145a5675e5718674cc
Duncan Hutty <dhutty@cisco.com>
2015-12-04 14:52:31 -0500

    Add support for molecule

--- omnibus-ansible-dk/config/projects/ansible-dk.rb
+++ omnibus-ansible-dk/config/projects/ansible-dk.rb
@@ -27,6 +27,7 @@ override :'ansible-lint',    version: "2.1.3"
 override :'ansible-toolkit', version: "1.3.2"
 override :appbundler,        version: "0.6.0"
 override :serverspec,        version: "v2.24.2"
+override :molecule,          version: "1.0.5"
 
 
 # Creates required build directories
@@ -42,6 +43,7 @@ dependency "awscli"
 dependency "ansible"
 dependency "ansible-lint"
 dependency "ansible-toolkit"
+dependency "molecule"
 
 # Ruby land
 dependency "ruby"
---
+++ omnibus-ansible-dk/config/software/molecule.rb
@@ -0,0 +1,16 @@
+name "molecule"
+default_version "1.0.5"
+
+dependency "python"
+dependency "pip"
+
+build do
+  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/embedded/bin\" #{name}==#{version}"
+
+  # This is an Officially Exposed Tool
+  [
+    'molecule',
+  ].each do |tool|      
+    link "#{install_dir}/embedded/bin/#{tool}", "#{install_dir}/bin"
+  end
+end
