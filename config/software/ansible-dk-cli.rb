58a3bdd08089671a0da485e47059365260732bf6
Josh Blancett <joshblancett@gmail.com>
2016-04-18 13:59:50 -0400

    updated software definition for ansible-dk cli

--- config/software/ansible-dk-cli.rb
+++ config/software/ansible-dk-cli.rb
@@ -1,8 +1,7 @@
 name "ansible-dk-cli"
 default_version "0.1.0" # This is just to silence a warning
-description "One day this might be a generic CLI tool; today it is a hack to make shell-init work"
+description "generic CLI tool for ansible-dk"
 
-# This isn't used yet (as we're a shell script, derp) but will be soon!
 dependency 'click'
 
 build do
